d7c2d9e40d3b56e1661c294248f4ee654a664d05
Julio Machado <juliocarlos00@hotmail.com>
2021-04-26 20:43:20 -0300

    add/ header responsibility

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,22 @@
 import { Avatar } from "@chakra-ui/avatar";
 import { Box, Flex, Text } from "@chakra-ui/layout";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Julio Machado</Text>
-                <Text color="gray.300" fontSize="small">juliocarlos00@hotmail.com</Text>
-            </Box>
-
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Julio Machado</Text>
+                    <Text color="gray.300"
+                        fontSize="small"
+                    >
+                        juliocarlos00@hotmail.com</Text>
+                </Box>
+            )}
             <Avatar size="md" name="Julio Machado" src="https://github.com/juliomchado.png" />
         </Flex>
     );
--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -31,6 +31,7 @@ export function SearchBox() {
                 placeholder="Buscar na plataforma"
                 _placeholder={{ color: 'gray.400' }}
             />
+            
             <Icon as={RiSearchLine} fontSize="20" />
         </Flex>
     );
