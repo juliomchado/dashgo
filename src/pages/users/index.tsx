a205835882cb23f675fe71aa3c2c65e51297c5a3
Julio Machado <juliocarlos00@hotmail.com>
2021-04-28 18:15:01 -0300

    add/ list responsive

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,16 @@
-import { Box, Button, Flex, Heading, Icon, Table, Th, Tr, Thead, Checkbox, Tbody, Td, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Icon, Table, Th, Tr, Thead, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { SideBar } from "../../components/Sidebar";
 
 export default function UserList() {
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true
+    });
+
+
     return (
         <Box>
             <Header />
@@ -34,17 +40,20 @@ export default function UserList() {
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" width="8">
+                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                                     <Checkbox colorScheme="pink" />
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
+                                {isWideVersion && (
+                                    <Th>Data de cadastro</Th>
+                                )}
+
                                 <Th width="8"></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4", "6"]}>
                                     <Checkbox colorScheme="pink" />
                                 </Td>
                                 <Td>
@@ -53,9 +62,9 @@ export default function UserList() {
                                         <Text fontSize="sm" color="gray.300">juliocarlos00@hotmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>04 de Abril, 2021</Td>
+                                {isWideVersion && (<Td>04 de Abril, 2021</Td>)}
                                 <Td>
-                                    <Button
+                                    {isWideVersion && (<Button
                                         as="a"
                                         size="sm"
                                         fontSize="sm"
@@ -66,11 +75,11 @@ export default function UserList() {
                                         }}
                                     >
                                         Editar
-                                    </Button>
+                                    </Button>)}
                                 </Td>
                             </Tr>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4", "6"]}>
                                     <Checkbox colorScheme="pink" />
                                 </Td>
                                 <Td>
@@ -79,9 +88,9 @@ export default function UserList() {
                                         <Text fontSize="sm" color="gray.300">juliocarlos00@hotmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>04 de Abril, 2021</Td>
+                                {isWideVersion && (<Td>04 de Abril, 2021</Td>)}
                                 <Td>
-                                    <Button
+                                    {isWideVersion && (<Button
                                         as="a"
                                         size="sm"
                                         fontSize="sm"
@@ -92,7 +101,7 @@ export default function UserList() {
                                         }}
                                     >
                                         Editar
-                                    </Button>
+                                    </Button>)}
                                 </Td>
                             </Tr>
                         </Tbody>
